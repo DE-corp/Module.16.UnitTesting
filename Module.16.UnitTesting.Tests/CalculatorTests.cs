ffcf9c284ac4c938b79a374e99abc25e3682ee26
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-15 17:58:43 +0300

    Добавьте файлы проекта.

---
+++ Module.16.UnitTesting.Tests/CalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Module._16.UnitTesting.Tests
+{
+    [TestFixture]
+    class CalculatorTests
+    {
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+    }
+}
---
+++ Module.16.UnitTesting/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Module._16.UnitTesting
+{
+    public class Calculator
+    {
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
