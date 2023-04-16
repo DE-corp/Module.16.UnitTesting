bbd0aa892ac8502c84ebc88181fb6af7b2c4e8ae
DE-corp <crazyHOPHOP@rambler.ru>
2023-04-16 19:44:27 +0300

    Задание 16.6.2

---
+++ Module.16.UnitTesting.Tests/FileWorkerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+
+namespace Module._16.UnitTesting.Tests
+{
+    public class FileWorkerTests
+    {
+        [Fact]
+        public void GetSafeFilenameMustReturnCorrectValue()
+        {
+            var fileWorkerTest = new FileWorker();
+            PrivateObject obj = new PrivateObject(fileWorkerTest);
+
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual("Folder________Name_", obj.Invoke("GetSafeFilename", @"Folder:?<>*/\_Name:"));
+
+        }
+    }
+}
---
+++ Module.16.UnitTesting/FileWorker.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace Module._16.UnitTesting
+{
+    public class FileWorker
+    {
+        private string GetSafeFilename(string filename)
+        {
+            return string.Join("_", filename.Split(Path.GetInvalidFileNameChars()));
+        }
+    }
+}
